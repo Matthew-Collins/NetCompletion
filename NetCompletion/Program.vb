d1b69403d6010f0d038873d92bbf3466e578355b
Matthew-Collins <matthew.collins@cwgsy.net>
2017-09-17 11:46:06 +0100

    Not Working Yet!

--- NetCompletion/Program.vb
+++ NetCompletion/Program.vb
@@ -5,9 +5,50 @@ Imports Microsoft.CodeAnalysis.Completion
 Imports Microsoft.CodeAnalysis.Host.Mef
 
 Module Program
-    Sub Main(args As String())
+    Private Enum Languages
+        None
+        VisualBasic
+        CSharp
+    End Enum
 
-        Dim Code As String =
+    Sub Main(Args As String())
+        Try
+
+            'Command Line Arguments
+            Dim Language As Languages = Languages.None
+            Dim Offset As Integer = 0
+            Dim Text As String = String.Empty
+            Dim InText As Boolean = True
+            For Each Arg In Args
+
+                'Language
+                If Arg.ToLower.StartsWith("-l:vb") Then
+                    Language = Languages.VisualBasic
+                    Debug.WriteLine("Language: Visual Basic")
+                ElseIf Arg.ToLower.StartsWith("-l:cs") Then
+                    Language = Languages.CSharp
+                    Debug.WriteLine("Language: CSharp")
+                End If
+
+                'Caret Position Offset
+                If Arg.ToLower.StartsWith("-offset:") Then
+                    Offset = CInt(Arg.Substring(8))
+                    Debug.WriteLine($"Offset: {Offset:#,##0}")
+                End If
+
+                'Code Text
+                If InText Then
+                    InText = False
+                    Text = Arg
+                    Debug.WriteLine($"Text:{Environment.NewLine}{Text}")
+                End If
+                If Arg.ToLower.StartsWith("-text") Then
+                    InText = True
+                End If
+
+            Next
+
+            Text =
 "Imports System
 
 Module Program
@@ -16,39 +57,60 @@ Module Program
     End Sub
 End Module"
 
-        Console.WriteLine(Code)
+            Offset = Text.IndexOf(".")
 
-        Dim CaretPosition = Code.IndexOf(".")
-        Console.WriteLine(Code.Substring(CaretPosition - 9, 10))
+            'Create Default Host Languges
+            Dim Assemblies As New List(Of Assembly) From {
+                Assembly.Load("Microsoft.CodeAnalysis"),
+                Assembly.Load("Microsoft.CodeAnalysis.Features")
+            }
 
-        'Dim assemblies = {Assembly.Load("Microsoft.CodeAnalysis"), Assembly.Load("Microsoft.CodeAnalysis.CSharp"), Assembly.Load("Microsoft.CodeAnalysis.Features"), Assembly.Load("Microsoft.CodeAnalysis.CSharp.Features")}
-        'Dim partTypes = MefHostServices.DefaultAssemblies.Concat(assemblies).Distinct().SelectMany(Function(x) x.GetTypes()).ToArray()
-        'Dim compositionContext = New ContainerConfiguration().WithParts(partTypes).CreateContainer()
-        'Dim host = MefHostServices.Create(compositionContext)
-        'Dim workspace = New AdhocWorkspace(host)
-        'Dim document = workspace.AddProject("TestProject", LanguageNames.CSharp).AddDocument("TestDocument.cs", "console.")
-        'Dim service = CompletionService.GetService(document)
+            'Add Specific Languages
+            Select Case Language
+                Case Languages.VisualBasic
+                    Assemblies.Add(Assembly.Load("Microsoft.CodeAnalysis.VisualBasic"))
+                    Assemblies.Add(Assembly.Load("Microsoft.CodeAnalysis.VisualBasic.Features"))
+                Case Languages.CSharp
+                    Assemblies.Add(Assembly.Load("Microsoft.CodeAnalysis.CSharp"))
+                    Assemblies.Add(Assembly.Load("Microsoft.CodeAnalysis.CSharp.Features"))
+            End Select
 
-        Dim assemblies = {Assembly.Load("Microsoft.CodeAnalysis"), Assembly.Load("Microsoft.CodeAnalysis.VisualBasic"), Assembly.Load("Microsoft.CodeAnalysis.Features"), Assembly.Load("Microsoft.CodeAnalysis.VisualBasic.Features")}
-        Dim partTypes = MefHostServices.DefaultAssemblies.Concat(assemblies).Distinct().SelectMany(Function(x) x.GetTypes()).ToArray()
-        Dim compositionContext = New ContainerConfiguration().WithParts(partTypes).CreateContainer()
-        Dim host = MefHostServices.Create(compositionContext)
-        Dim workspace = New AdhocWorkspace(host)
-        Dim document = workspace.AddProject("TestProject", LanguageNames.VisualBasic).AddDocument("TestDocument.vb", Code)
-        Dim service = CompletionService.GetService(document)
+            'Configure MefHost
+            Dim Types = MefHostServices.DefaultAssemblies.Concat(Assemblies).Distinct().SelectMany(Function(x) x.GetTypes()).ToArray()
+            Dim Container = New ContainerConfiguration().WithParts(Types).CreateContainer()
+            Dim Host = MefHostServices.Create(Container)
 
-        Console.WriteLine("Starting...")
+            'Create Workspace
+            Dim Workspace = New AdhocWorkspace(Host)
+            Dim Project = Workspace.AddProject("TestProject", LanguageNames.VisualBasic)
+            Dim Document = Project.AddDocument("TestDocument.vb", Text)
 
-        If service IsNot Nothing Then
-            Dim List = service.GetCompletionsAsync(document, 0).Result
-            If List IsNot Nothing Then
-                For Each Item In List.Items
-                    Console.WriteLine(Item.DisplayText)
+            'Get Code Completion
+            Dim Service = CompletionService.GetService(Document)
+            If Service IsNot Nothing Then
+                For Offset = 0 To Text.Length
+                    'Debug.WriteLine("")
+                    Debug.WriteLine($"Offset: {Offset}")
+                    Dim List = Service.GetCompletionsAsync(Document, Offset).Result
+                    If List IsNot Nothing Then
+                        'Debug.WriteLine($"Total: {List.Items.Count}")
+
+                        Dim Items As New List(Of String)
+
+                        For Each Item In List.Items
+                            Console.WriteLine(Item.DisplayText)
+                            'Debug.WriteLine(Item.DisplayText)
+                            Items.Add(Item.DisplayText)
+                        Next
+
+                        Debug.WriteLine(String.Join(",", Items))
+
+                    End If
                 Next
             End If
-        End If
 
-        Console.WriteLine("Finished!")
-
+        Catch ex As Exception
+            Debug.WriteLine(ex.ToString)
+        End Try
     End Sub
 End Module
